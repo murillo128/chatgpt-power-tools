0f3a29c4a0d1073fe1e5e1c4c311b07fc61de201
Sergio Garcia Murillo <sergio.garcia.murillo@gmail.com>
2026-06-03 15:44:12 +0200

    Add Vite config

---
+++ extension/vite.config.ts
@@ -0,0 +1,19 @@
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  build: {
+    outDir: "dist",
+    emptyOutDir: true,
+    sourcemap: true,
+    rollupOptions: {
+      input: {
+        content: "src/content.ts",
+      },
+      output: {
+        entryFileNames: "[name].js",
+        chunkFileNames: "assets/[name]-[hash].js",
+        assetFileNames: "assets/[name]-[hash][extname]",
+      },
+    },
+  },
+});
